5d6c70aa5a01cc2ccffa68285b91d8ff7be002cc
Ildus K <i.kurbangaliev@gmail.com>
2015-06-22 18:19:29 +0300

    circle

--- corr.py
+++ corr.py
@@ -11,16 +11,20 @@ def get_data(correlated=True, count=1000, start=1, end=1000):
 		
 	return arr2
 
+def get_circle_data(correlated=True, radius=100, x0=0, y0=0):
+	f = np.linspace(0, 2*np.pi, 628)
+	X, Y = np.cos(f), np.sin(f)
+	X = np.array([x0 + radius * x for x in X])
+	Y = np.array([y0 + radius * y for y in Y])
+	if not correlated:
+		Y = np.array([y + random.randint(1, 100) - 50 for y in Y])
+	
+	return zip(X, Y)
+
 def cumulative_entropy_hist(arr, bins=10):
 	hist, bin_edges = np.histogram(arr, density=True, bins=bins)
 	cum_distrib = np.cumsum(hist*np.diff(bin_edges))
-	try:
-		cum_entropy_args = [cd * math.log(cd * 1.0, 2) for cd in cum_distrib]
-	except Exception as e:
-		print(e)
-		print(cum_distrib)
-		raise
-
+	cum_entropy_args = [cd * math.log(cd * 1.0, 2) for cd in cum_distrib]
 	return -sum(cum_entropy_args * np.diff(bin_edges))
 
 def cumulative_entropy_arr(arr):
@@ -50,3 +54,11 @@ if __name__ == "__main__":
 		print("Correlated")
 		data = get_data(correlated=True, start=start, end=end)
 		print("{0}-{1} {2}".format(start, end, corr(data)))
+	
+	print("\nNot correlated circle")
+	data = get_circle_data(correlated=False)
+	print("{0}-{1} {2}".format(start, end, corr(data)))
+
+	print("Correlated")
+	data = get_circle_data(correlated=True)
+	print("{0}-{1} {2}".format(start, end, corr(data)))
